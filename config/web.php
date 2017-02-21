2ae78e26dca7d19620c06766814cb005f5081c23
Vladimir Sanin <vo.saan@gmail.com>
2017-02-21 20:51:26 +0200

    pretty URLs enabled

--- config/web.php
+++ config/web.php
@@ -10,6 +10,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'pacaivrfMjQ5vA6qZVXwqIMQ3V2JvVas',
+            'baseUrl' => ''
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -38,14 +39,14 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
