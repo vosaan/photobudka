e25506754d23afb22fcf87a218e01903dd9c3c7d
Vladimir Sanin <vo.saan@gmail.com>
2017-02-21 21:16:46 +0200

    admin module created and plugged

--- config/web.php
+++ config/web.php
@@ -49,6 +49,12 @@ $config = [
 
     ],
     'params' => $params,
+
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
+    ],
 ];
 
 if (YII_ENV_DEV) {
