86dd4b585bbd2ae746eebe2fa5ed35057a441a9b
Vladimir Sanin <vo.saan@gmail.com>
2017-02-23 18:10:59 +0200

    1. Add css for admin module 2. Login page tuning

--- modules/admin/views/user/login.php
+++ modules/admin/views/user/login.php
@@ -2,11 +2,17 @@
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 ?>
+<div class="login-form">
+    <div class="form-container">
+        <?php $form = ActiveForm::begin()?>
+        <?= $form->field($model, 'username')->textInput()?>
+        <?= $form->field($model, 'password')->passwordInput()?>
+        <?=Html::submitButton('Login', ['class' => 'btn-primary btn login-button'])?>
+        <?php ActiveForm::end()?>
 
-<h1>login</h1>
+        <p class="go-home">
+            <?=Html::a('Go to public part of site', [Yii::$app->homeUrl])?>
+        </p>
+    </div>
 
-<?php $form = ActiveForm::begin()?>
-<?= $form->field($model, 'username')->textInput()?>
-<?= $form->field($model, 'password')->passwordInput()?>
-<?=Html::submitButton('Login', ['class' => 'btn-primary btn'])?>
-<?php ActiveForm::end()?>
+</div>
